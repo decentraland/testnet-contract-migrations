026129d74e9ab52fb3aacb6bc98e7a85f01e44a4
Nando <24811313+fzavalia@users.noreply.github.com>
2023-06-22 16:33:48 -0300

    fix: Use ethers

--- scripts/foo.ts
+++ scripts/foo.ts
@@ -1,35 +1,13 @@
-import {
-  Abi,
-  createClient,
-  createPublicClient,
-  createTestClient,
-  createWalletClient,
-  custom,
-  getContract,
-  http,
-  parseAbi,
-  parseGwei,
-} from "viem";
-import { mainnet, localhost } from "viem/chains";
-import fetch, { Headers, Request, Response } from "node-fetch";
 import fs from "fs";
 import path from "path";
 import { outputPath } from "./utils/paths";
 import ganache, { EthereumProvider } from "ganache";
+import { ethers } from "ethers";
 
 async function main() {
-  // @ts-ignore
-  globalThis.fetch = fetch;
-  // @ts-ignore
-  globalThis.Headers = Headers;
-  // @ts-ignore
-  globalThis.Request = Request;
-  // @ts-ignore
-  globalThis.Response = Response;
-
   const server = ganache.server();
 
-  const provider = await new Promise<EthereumProvider>((resolve, reject) => {
+  const ganacheProvider = await new Promise<EthereumProvider>((resolve, reject) => {
     server.listen(8545, (err) => {
       if (err) {
         return reject(err);
@@ -39,73 +17,94 @@ async function main() {
     });
   });
 
-  const files = fs.readdirSync(path.join(outputPath, "getContractCreationDataFromEtherscan"));
+  const fileNames = fs.readdirSync(path.join(outputPath, "getContractCreationDataFromEtherscan"));
 
-  for (const file of [files.find((file) => file.includes("MANAToken"))!]) {
-    const filePath = path.join(outputPath, "getContractCreationDataFromEtherscan", file);
+  for (const fileName of [fileNames.find((file) => file.includes("MANAToken"))!]) {
+    const file = path.join(outputPath, "getContractCreationDataFromEtherscan", fileName);
 
-    const data = JSON.parse(fs.readFileSync(filePath, "utf-8"))[0];
+    const contractCreationData = JSON.parse(fs.readFileSync(file, "utf-8"))[0];
 
-    const mainnetPublicClient = createPublicClient({
-      transport: http(),
-      chain: mainnet,
-    });
+    const mainnetProvider = ethers.getDefaultProvider(1);
 
-    const originalCreationTransaction = await mainnetPublicClient.getTransaction({ hash: data.txHash });
+    const transaction = await mainnetProvider.getTransaction(contractCreationData.txHash);
 
-    const walletClient = createWalletClient({
-      transport: custom(provider),
-    });
+    if (!transaction) {
+      throw new Error("Transaction not found");
+    }
 
-    const publicClient = createPublicClient({
-      transport: custom(provider),
-    });
+    const { data } = transaction;
 
-    const testClient = createTestClient({
-      transport: custom(provider),
-      mode: "ganache",
-    });
+    const abiPath = path.join(outputPath, "getContractAbiFromEtherscan", fileName);
 
-    const [account] = await walletClient.getAddresses();
+    const abi = JSON.parse(fs.readFileSync(abiPath, "utf-8"));
 
-    const hash = await walletClient.sendTransaction({
-      account,
-      chain: localhost,
-      data: originalCreationTransaction.input,
-      gas: originalCreationTransaction.gas,
-    });
+    const provider = new ethers.BrowserProvider(ganacheProvider);
 
-    const newCreationTransactionReceipt = await publicClient.waitForTransactionReceipt({ hash });
+    const [account1, _account2, _account3] = await provider.listAccounts();
 
-    await testClient.mine({ blocks: 1 });
+    console.log(account1)
 
-    // console.log(newCreationTransactionReceipt);
+    const factory = new ethers.ContractFactory(abi, data, account1);
 
-    await publicClient.simulateContract({
-      abi: JSON.parse(
-        JSON.parse(fs.readFileSync(path.join(outputPath, "getContractSourceCodeFromEtherscan", file), "utf-8"))[0].ABI
-      ),
-      address: newCreationTransactionReceipt.contractAddress!,
-      functionName: 'balanceOf',
-      args: [account]
-    })
+    const contract = await factory.deploy();
 
-    // const contract = getContract({
-    //   address: newCreationTransaction.contractAddress!,
+    await contract.waitForDeployment();
+
+    
+
+    // const walletClient = createWalletClient({
+    //   transport: custom(mainnetProvider),
+    // });
+
+    // const publicClient = createPublicClient({
+    //   transport: custom(mainnetProvider),
+    // });
+
+    // const testClient = createTestClient({
+    //   transport: custom(mainnetProvider),
+    //   mode: "ganache",
+    // });
+
+    // const [account] = await walletClient.getAddresses();
+
+    // const hash = await walletClient.sendTransaction({
+    //   account,
+    //   chain: localhost,
+    //   data: originalCreationTransaction.input,
+    //   gas: originalCreationTransaction.gas,
+    // });
+
+    // const newCreationTransactionReceipt = await publicClient.waitForTransactionReceipt({ hash });
+
+    // await testClient.mine({ blocks: 1 });
+
+    // // console.log(newCreationTransactionReceipt);
+
+    // await publicClient.simulateContract({
     //   abi: JSON.parse(
     //     JSON.parse(fs.readFileSync(path.join(outputPath, "getContractSourceCodeFromEtherscan", file), "utf-8"))[0].ABI
     //   ),
-    //   publicClient,
-    //   walletClient,
+    //   address: newCreationTransactionReceipt.contractAddress!,
+    //   functionName: "balanceOf",
+    //   args: [account],
     // });
 
-    // console.log(contract)
+    // // const contract = getContract({
+    // //   address: newCreationTransaction.contractAddress!,
+    // //   abi: JSON.parse(
+    // //     JSON.parse(fs.readFileSync(path.join(outputPath, "getContractSourceCodeFromEtherscan", file), "utf-8"))[0].ABI
+    // //   ),
+    // //   publicClient,
+    // //   walletClient,
+    // // });
 
-    // console.log(contract);
+    // // console.log(contract)
 
-    // console.log(contract.read);
+    // // console.log(contract);
 
-    // console.log(transaction2);
+    // // console.log(contract.read);
+
+    // // console.log(transaction2);
   }
 
   server.close();
