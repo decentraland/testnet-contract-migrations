b7734b47c1fe583c36c1bda59f2318da0699c451
Nando <24811313+fzavalia@users.noreply.github.com>
2023-06-28 17:15:36 -0300

    fix: Path

--- scripts/common/paths.ts
+++ scripts/common/paths.ts
@@ -1,6 +1,6 @@
 import path from "path";
 
-export const rootDir = path.resolve(__dirname, "..");
+export const rootDir = path.resolve(__dirname, "../..");
 export const prepareDownloadsDir = path.join(rootDir, "scripts/prepare/.downloads");
 export const sourceCodesDir = path.join(prepareDownloadsDir, "sourceCodes");
 export const creationCodesDir = path.join(prepareDownloadsDir, "creationCodes");
