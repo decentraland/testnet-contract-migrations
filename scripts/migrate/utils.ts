6eb6071c1dfd5c82ae01cd65380893b2e608adbd
Nando <24811313+fzavalia@users.noreply.github.com>
2023-06-28 17:17:31 -0300

    feat: Move load origins data to utils

--- scripts/migrate/utils.ts
+++ scripts/migrate/utils.ts
@@ -1,4 +1,8 @@
-import { ChainId } from "./types";
+import fs from "fs";
+import { creationCodesDir, sourceCodesDir } from "../common/paths";
+import { ContractName } from "../common/types";
+import { deploymentOrder } from "./config";
+import { ChainId, OriginContractData, SourceCodeData } from "./types";
 
 export const GANACHE_RPC_URL = "http://localhost:8545";
 export const SEPOLIA_RPC_URL = `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`;
@@ -11,5 +15,31 @@ export function getRpcUrl(chainId: ChainId): string {
       return SEPOLIA_RPC_URL;
     default:
       throw new Error("Chain ID not supported");
+  }
+}
+
+export function loadOriginContractsData() {
+  const originContractDataMap = new Map<ContractName, OriginContractData>();
+
+  for (const contractName of deploymentOrder) {
+    console.log("Loading contract data for", ContractName[contractName]);
+
+    const sourceCode = load<SourceCodeData>(sourceCodesDir, contractName);
+    const creationCode = removeConstructorArgs(load<[string]>(creationCodesDir, contractName)[0], sourceCode);
+
+    originContractDataMap.set(contractName, {
+      creationCode,
+      sourceCode,
+    });
+  }
+
+  return originContractDataMap;
+
+  function load<T>(dir: string, contractName: ContractName): T {
+    return JSON.parse(fs.readFileSync(`${dir}/${ContractName[contractName]}.json`, `utf-8`));
+  }
+
+  function removeConstructorArgs(creationCode: string, sourceCodeData: SourceCodeData): string {
+    return creationCode[0].replace(new RegExp(`${sourceCodeData.ConstructorArguments}$`), "");
   }
 }
