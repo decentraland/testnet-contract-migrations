2e9e7142fb41a9bfc251bfa223716040148b5826
Nando <24811313+fzavalia@users.noreply.github.com>
2023-06-28 17:17:50 -0300

    fix: Import

--- scripts/migrate/verifyContract.ts
+++ scripts/migrate/verifyContract.ts
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-import { SourceCodeData } from "../common/types";
+import { SourceCodeData } from "./types";
 
 export async function verifyContract(
   contractAddress: string,
