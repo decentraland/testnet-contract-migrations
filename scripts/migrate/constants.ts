6f30f692c0b79cf157a6e639aab8580f4c00cec3
Nando <24811313+fzavalia@users.noreply.github.com>
2023-06-28 17:10:09 -0300

    fix: Move constants

--- scripts/migrate/constants.ts
+++
@@ -1,2 +0,0 @@
-export const GANACHE_RPC_URL = "http://localhost:8545";
-export const SEPOLIA_RPC_URL = `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`;
--- scripts/migrate/utils.ts
+++ scripts/migrate/utils.ts
@@ -1,5 +1,7 @@
-import { GANACHE_RPC_URL, SEPOLIA_RPC_URL } from "./constants";
 import { ChainId } from "../common/types";
+
+export const GANACHE_RPC_URL = "http://localhost:8545";
+export const SEPOLIA_RPC_URL = `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`;
 
 export function getRpcUrl(chainId: ChainId): string {
   switch (chainId) {
