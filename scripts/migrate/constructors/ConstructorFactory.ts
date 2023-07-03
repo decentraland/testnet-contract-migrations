850ce488b8ff45107945fd3626afcf4aba97a1f7
Nando <24811313+fzavalia@users.noreply.github.com>
2023-07-03 16:40:42 -0300

    feat: Get Constructor args hex

--- scripts/migrate/constructors/ConstructorFactory.ts
+++ scripts/migrate/constructors/ConstructorFactory.ts
@@ -1,13 +1,28 @@
 import { ethers } from "ethers";
 import { ContractName } from "../../common/types";
-import { deployedContractAddresses } from "../config";
+import { deployedContractAddresses, originContractsData } from "../config";
 
 export abstract class ConstructorFactory {
+  abstract name: ContractName;
+
   abstract getConstructorArgs(): ethers.ContractMethodArgs<any[]>;
 
-  getAddress = (contract: ContractName): string => {
+  getConstructorArgsHex = (): string => {
+    const args = this.getConstructorArgs();
+    const abi = this.getAbi(this.name);
+
+    return new ethers.Interface(abi).encodeDeploy(args);
+  };
+
+  protected getAddress = (contract: ContractName): string => {
     const address = deployedContractAddresses.get(contract);
     if (!address) throw new Error(`Address not found for ${contract}`);
     return address;
+  };
+
+  private getAbi = (contract: ContractName): string => {
+    const abi = originContractsData.get(contract)?.sourceCode.ABI;
+    if (!abi) throw new Error(`ABI not found for ${contract}`);
+    return abi;
   };
 }
--- scripts/migrate/constructors/impl/EstateProxyConstructorFactory.ts
+++ scripts/migrate/constructors/impl/EstateProxyConstructorFactory.ts
@@ -1,8 +1,10 @@
-import { ContractMethodArgs, getAddress } from "ethers";
+import { ContractMethodArgs } from "ethers";
 import { ConstructorFactory } from "../ConstructorFactory";
 import { ContractName } from "../../../common/types";
 
 export class EstateProxyConstructorFactory extends ConstructorFactory {
+  name: ContractName = ContractName.EstateProxy;
+
   getConstructorArgs(): ContractMethodArgs<any[]> {
     return [this.getAddress(ContractName.EstateRegistry)];
   }
