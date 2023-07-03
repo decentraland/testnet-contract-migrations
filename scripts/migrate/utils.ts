024a7628c0aae2aee40a12d93c87f98cde84bc1f
Nando <24811313+fzavalia@users.noreply.github.com>
2023-07-03 11:08:28 -0300

    fix: Move verifyContract to utils

--- scripts/migrate/utils.ts
+++ scripts/migrate/utils.ts
@@ -43,3 +43,37 @@ export function loadOriginContractsData() {
     return creationCode[0].replace(new RegExp(`${sourceCodeData.ConstructorArguments}$`), "");
   }
 }
+
+export async function verifyContract(
+  contractAddress: string,
+  sourceCodeData: SourceCodeData,
+  constructorArguments: string
+) {
+  const parameters = new URLSearchParams({
+    apikey: process.env.ETHERSCAN_API_KEY!,
+    module: "contract",
+    action: "verifysourcecode",
+    contractaddress: contractAddress,
+    sourceCode: sourceCodeData.SourceCode,
+    codeformat: "solidity-single-file",
+    contractname: sourceCodeData.ContractName,
+    compilerversion: sourceCodeData.CompilerVersion,
+    optimizationUsed: sourceCodeData.OptimizationUsed,
+    runs: sourceCodeData.Runs,
+    constructorArguements: constructorArguments,
+  });
+
+  const res = await fetch("https://api-sepolia.etherscan.io/api", {
+    method: "post",
+    body: parameters,
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  });
+
+  const json = await res.json();
+
+  if (json.status !== "1") {
+    throw new Error(
+      `Failed to verify contract. status: ${json.status}, message: ${json.message}, result: ${json.result}`
+    );
+  }
+}
