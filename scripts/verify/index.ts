1da4a07c2e6db5b6ee50ebaeda49119cc03e0c0d
Nando <24811313+fzavalia@users.noreply.github.com>
2023-07-05 16:18:06 -0300

    feat: Sepolia deployments

---
+++ scripts/verify/index.ts
@@ -0,0 +1,45 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { deployedContractConstructorHexes, deploymentOrder } from "../migrate/config";
+import { getAddress, getSourceCodeData } from "../migrate/utils";
+import { ContractName } from "../common/types";
+
+async function main() {
+  for (const contractName of deploymentOrder) {
+    console.log(`\n--- ${ContractName[contractName]} ---\n`);
+
+    // Wait a second to prevent rate limiting
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const sourceCodeData = getSourceCodeData(contractName);
+
+    const constructorHex = deployedContractConstructorHexes.get(contractName) ?? "";
+
+    const parameters = new URLSearchParams({
+      apikey: process.env.ETHERSCAN_API_KEY!,
+      module: "contract",
+      action: "verifysourcecode",
+      contractaddress: getAddress(contractName),
+      sourceCode: sourceCodeData.SourceCode,
+      codeformat: "solidity-single-file",
+      contractname: sourceCodeData.ContractName,
+      compilerversion: sourceCodeData.CompilerVersion,
+      optimizationUsed: sourceCodeData.OptimizationUsed,
+      runs: sourceCodeData.Runs,
+      constructorArguements: constructorHex.replace("0x", ""),
+    });
+
+    const res = await fetch("https://api-sepolia.etherscan.io/api", {
+      method: "post",
+      body: parameters,
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    });
+
+    const json = await res.json();
+
+    console.log(json.status, json.result);
+  }
+}
+
+main();
