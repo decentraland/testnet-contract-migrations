672dd8c646df7d7f191a24e709fb9c5d0753c8ba
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2023-07-10 12:10:38 -0300

    fix: Verification for standard json input source code (#7)
    * fix: Verification for standard json input source code
    
    * fix: Add comments

--- scripts/verify/index.ts
+++ scripts/verify/index.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import fetch from 'node-fetch'
+import fetch from "node-fetch";
 import { deployedContractConstructorHexes, deploymentOrder } from "../migrate/config";
 import { getAddress, getSourceCodeData } from "../migrate/utils";
 import { ContractName } from "../common/types";
@@ -17,14 +17,42 @@ async function main() {
 
     const constructorHex = deployedContractConstructorHexes.get(contractName) ?? "";
 
+    let sourceCode = sourceCodeData.SourceCode;
+    let contractFile: string | undefined;
+
+    // All source codes in this format start with {{\r\n
+    const isStandardJsonInput = sourceCodeData.SourceCode.startsWith("{{\r\n");
+
+    if (isStandardJsonInput) {
+      // For it to be a valid JSON I need to remove the wrapping {}
+      sourceCode = sourceCode.slice(1, -1);
+
+      const sourceCodeJson = JSON.parse(sourceCode);
+
+      // This source code format requires that the Contract Name is prefixed with the name of the file containing it.
+      // To do this I need find the file that contains the main contract
+      for (const key in sourceCodeJson.sources) {
+        if ((sourceCodeJson.sources[key].content as string).includes(`contract ${sourceCodeData.ContractName}`)) {
+          contractFile = key;
+          break;
+        }
+      }
+
+      if (!contractFile) {
+        throw new Error(`Contract name not found in ${ContractName[contractName]} source code`);
+      }
+    }
+
     const parameters = new URLSearchParams({
       apikey: process.env.ETHERSCAN_API_KEY!,
       module: "contract",
       action: "verifysourcecode",
       contractaddress: getAddress(contractName),
-      sourceCode: sourceCodeData.SourceCode,
-      codeformat: "solidity-single-file",
-      contractname: sourceCodeData.ContractName,
+      sourceCode,
+      codeformat: isStandardJsonInput ? "solidity-standard-json-input" : "solidity-single-file",
+      contractname: isStandardJsonInput
+        ? `${contractFile}:${sourceCodeData.ContractName}`
+        : sourceCodeData.ContractName,
       compilerversion: sourceCodeData.CompilerVersion,
       optimizationUsed: sourceCodeData.OptimizationUsed,
       runs: sourceCodeData.Runs,
