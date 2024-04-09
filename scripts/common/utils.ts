4463decfa6b55db50c21d9b20fe06da1c59db674
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-04-09 14:59:33 -0300

    feat: Add common utils to fetch origin chain id and target chain id

---
+++ scripts/common/utils.ts
@@ -0,0 +1,35 @@
+import { ChainId } from "./types"
+
+export const originChainId: ChainId = (() => {
+  const num = Number(process.env.ORIGIN_CHAIN_ID ?? ChainId.MAINNET)
+
+  if (!ChainId[num]) {
+    throw new Error('Invalid ORIGIN_CHAIN_ID')
+  }
+
+  return num
+})()
+
+export const targetChainId: ChainId = (() => {
+  const env = process.env.TARGET_CHAIN_ID;
+
+  if (!env) {
+    throw new Error("TARGET_CHAIN_ID env var not found");
+  }
+
+  const num = Number(env);
+
+  if (!ChainId[num]) {
+    throw new Error("Invalid TARGET_CHAIN_ID");
+  }
+
+  return num;
+})();
+
+export function isEthereumNetwork(): boolean {
+  return [ChainId.MAINNET, ChainId.GOERLI, ChainId.SEPOLIA].includes(originChainId)
+}
+
+export function isPolygonNetwork(): boolean {
+  return [ChainId.POLYGON, ChainId.MUMBAI, ChainId.AMOY].includes(originChainId)
+}
