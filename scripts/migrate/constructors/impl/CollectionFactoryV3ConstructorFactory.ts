23cdaa8cb630f5ab814b1a431d5a7d2fd1151728
Gabriel Díaz <git@subsme.slmail.me>
2024-04-12 10:04:10 -0300

    fix: Update CollectionFactoryV3 constructor (#20)

--- scripts/migrate/constructors/impl/CollectionFactoryV3ConstructorFactory.ts
+++ scripts/migrate/constructors/impl/CollectionFactoryV3ConstructorFactory.ts
@@ -6,9 +6,7 @@ import { ConstructorFactory } from "../ConstructorFactory";
 export class CollectionFactoryV3ConstructorFactory extends ConstructorFactory {
   name: ContractName = ContractName.CollectionFactoryV3;
 
-  async getConstructorArgs(signers: ethers.Signer[]): Promise<ContractMethodArgs<any[]>> {
-    const owner = await signers[0].getAddress();
-
-    return [owner, getAddress(ContractName.Forwarder)];
+  async getConstructorArgs(_signers: ethers.Signer[]): Promise<ContractMethodArgs<any[]>> {
+    return [getAddress(ContractName.Forwarder), getAddress(ContractName.UpgradeableBeacon)];
   }
 }
